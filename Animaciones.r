6bbf172b9fb2d68b81067c13cf79b7fd2d1d4d32
MarianaMonrooy <a01653716@itesm.mx>
2021-05-13 14:05:04 -0600

    Update Animaciones.r
    codigo

---
+++ Animaciones.r
@@ -0,0 +1,33 @@
+# Animacionesenr
+#mariana
+#Borrar todas las variables existentes
+
+rm(list=ls())
+
+#Cargar libreria para graficar
+library(ggplot2)
+#Cargar libreria para dato
+library(gapminder)
+
+#asignar a la variablee datos la tabla de gapminder
+datos<-gapminder
+#Crear gráfico
+grafico<-ggplot(
+  datos,
+  aes(x=gdpPercap, y=lifeExp, colour=continent)
+) + geom_point(aes(size=pop), alpha=0.8) +
+  theme_minimal()+
+  theme(legend.position="bottom")+guides(size="none")+
+  labs(x="PIB per Capita", y="Esperanza de vida", col="")
+
+#Mostrar gráfico
+grafico
+
+#cargar librerias para animar la grafica
+library(gganimate)
+library(gifski)
+grafico <- grafico +
+  transition_time(year)
+labs(title="Año:{frame_time}")
+
+grafico
